452180c1d3c378f7781a967f8470c398f5354cbe
Marc Rozendaal <rozendaalmarc@gmail.com>
2022-12-16 16:36:20 +0100

    Added boid behaviour

--- Boid.cs
+++ Boid.cs
@@ -16,7 +16,7 @@ public class Boid {
     private float viewDist;
     private float protectedDist;
 
-    private Polygon body;
+    public Polygon body;
 
     private Flock flock;
 
@@ -26,7 +26,10 @@ public class Boid {
         body = GenerateBody();
         
         position = _pos;
-        //velocity = Vector2.random;
+        velocity = Vector2.RandomVector(-0.5f, 0.5f);
+
+        body.position = _pos;
+        body.rotation = -2*Vector2.Angle(velocity, Vector2.right);
 
         separationFactor = _seperationFactor;
         alignmentFactor = _alignmentFactor;
@@ -38,6 +41,56 @@ public class Boid {
     }
 
     public void Update() {
+
+        Vector2 close = Vector2.zero;
+        Vector2 velAvg = Vector2.zero;
+        Vector2 posAvg = Vector2.zero;
+        int neighbouringBoids = 0;
+
+        foreach(Boid b in flock.boids) {
+            if(b != this) {
+                if(Vector2.Distance(position, b.position) <= protectedDist) {
+                    close += (position - b.position);
+                }
+                if(Vector2.Distance(position, b.position) <= viewDist) {
+                    velAvg += b.velocity;
+                    posAvg += b.position;
+                    neighbouringBoids++;
+                }
+            }
+        }
+
+        if(MathF.Abs((flock.rightBounds[1].x - flock.rightBounds[0].x) * (flock.rightBounds[0].y - position.y) - (flock.rightBounds[0].x - position.x) * (flock.rightBounds[1].y - flock.rightBounds[0].y)) / Vector2.Distance(flock.rightBounds[0], flock.rightBounds[1]) <= protectedDist) {
+            close -= Vector2.right;
+        }
+        if(MathF.Abs((flock.leftBounds[1].x - flock.leftBounds[0].x) * (flock.leftBounds[0].y - position.y) - (flock.leftBounds[0].x - position.x) * (flock.leftBounds[1].y - flock.leftBounds[0].y)) / Vector2.Distance(flock.leftBounds[0], flock.leftBounds[1]) <= protectedDist) {
+            close += Vector2.right;
+        }
+        if(MathF.Abs((flock.upperBounds[1].x - flock.upperBounds[0].x) * (flock.upperBounds[0].y - position.y) - (flock.upperBounds[0].x - position.x) * (flock.upperBounds[1].y - flock.upperBounds[0].y)) / Vector2.Distance(flock.upperBounds[0], flock.upperBounds[1]) <= protectedDist) {
+            close -= Vector2.up;
+        }
+        if(MathF.Abs((flock.bottomBounds[1].x - flock.bottomBounds[0].x) * (flock.bottomBounds[0].y - position.y) - (flock.bottomBounds[0].x - position.x) * (flock.bottomBounds[1].y - flock.bottomBounds[0].y)) / Vector2.Distance(flock.bottomBounds[0], flock.bottomBounds[1]) <= protectedDist) {
+            close += Vector2.up;
+        }
+
+        if(neighbouringBoids > 0) {
+            velAvg /= neighbouringBoids;
+            posAvg /= neighbouringBoids;
+        }
+
+        velocity += close * separationFactor;
+        velocity += (velAvg - velocity) * alignmentFactor;
+        velocity += (posAvg - position) * cohesionFactor;
+
+        position += velocity;
+
+        body.position = position;
+        if(velocity.y < 0) {
+            body.rotation = -Vector2.Angle(Vector2.right, velocity);
+        }
+        else {
+            body.rotation = Vector2.Angle(Vector2.right, velocity);
+        }
 
     }
 
--- Flock.cs
+++ Flock.cs
@@ -3,13 +3,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using ZaephusEngine;
+using Random = ZaephusEngine.Random;
 
 public class Flock {
+    
+    private float separationFactor = 0.001f;
+    private float alignmentFactor = 0.0005f;
+    private float cohesionFactor = 0.00005f;
+
+    private float viewDist = 150;
+    private float protectedDist = 50;
 
     public List<Boid> boids = new List<Boid>();
 
-    public Flock() {}
+    public Vector2[] rightBounds = new Vector2[] {
+        new Vector2(Window.WIDTH, 0),
+        new Vector2(Window.WIDTH, Window.HEIGHT)
+    };
+    public Vector2[] leftBounds = new Vector2 [] {
+        new Vector2(0, 0),
+        new Vector2(0, Window.HEIGHT)
+    };
+    public Vector2[] upperBounds = new Vector2 [] {
+        new Vector2(0, 0),
+        new Vector2(Window.WIDTH, 0)
+    };
+    public Vector2[] bottomBounds = new Vector2 [] {
+        new Vector2(0, Window.HEIGHT),
+        new Vector2(Window.WIDTH, Window.HEIGHT)
+    };
 
-    public void Update() {}
+    public Flock(int _boidAmount) {
+        for(int i = 0; i < _boidAmount; i++) {
+            boids.Add(new Boid(
+                this,
+                new Vector2(Random.Range(0, Window.WIDTH), Random.Range(0, Window.HEIGHT)),
+                separationFactor,
+                alignmentFactor,
+                cohesionFactor,
+                viewDist,
+                protectedDist
+            ));
+        }
+    }
+
+    public void Update() {
+        foreach(Boid b in boids) {
+            b.Update();
+            //b.body.rotation += 0.01f;
+        }
+    }
 
 }
