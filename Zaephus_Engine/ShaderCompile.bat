7928b1515ed98b5ef8f26a894f08d6d5eba673a5
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-11-05 15:03:48 +0100

    Completed drawing a triangle
    Completed vulkan-tutorial.com until Drawing a triangle / Drawing / Rendering and presentation

--- Zaephus_Engine/ShaderCompile.bat
+++ Zaephus_Engine/ShaderCompile.bat
@@ -1,9 +1,9 @@
 @echo off
 
 for /r %1 %%g in (*.frag) do (
-	%2\Bin\glslc.exe %%g -o %%~ng.frag.spv
+	%2\Bin\glslc.exe %%g -o %%~pg%%~ng_frag.spv
 )
 
 for /r %1 %%g in (*.vert) do (
-	%2\Bin\glslc.exe %%g -o %%~ng.vert.spv
+	%2\Bin\glslc.exe %%g -o %%~pg%%~ng_vert.spv
 )
